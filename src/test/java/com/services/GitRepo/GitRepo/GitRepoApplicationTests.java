2fb63da217e2a2b6a16726bae58ff47fbe84b8b6
sindhu <sindhurajendhar967@gmail.com>
2022-08-02 12:59:57 +0530

    commit

---
+++ src/test/java/com/services/GitRepo/GitRepo/GitRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.GitRepo.GitRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
